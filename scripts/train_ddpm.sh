b1f1234430bd97e96d3442de13893974a442b509
JoGo175 <jorgefernando1998@hotmail.com>
2024-05-13 10:34:02 +0200

    ddpm run files

--- scripts/train_ddpm.sh
+++ scripts/train_ddpm.sh
@@ -8,22 +8,18 @@ conda activate treevae
 dataset="cifar10"
 O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
 
-# sbatch --time=100:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python train_ddpm.py --config_name $dataset"
+sbatch --time=100:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python train_ddpm.py --config_name $dataset"
 
 
-# path for vae_chkpt_path strings to the trained TreeVAE models
-path='models/experiments/cifar10/20240506-143804_675eb'
+# # path for vae_chkpt_path strings to the trained TreeVAE models
+# path='models/experiments/cifar10/20240506-143804_675eb'
 
-# directory to save the results
-base_results_dir='/cluster/work/vogtlab/Group/jogoncalves/results/cifar10/'
+# # directory to save the results
+# base_results_dir='/cluster/work/vogtlab/Group/jogoncalves/results/cifar10/'
 
-# loop over seeds and vae_chkpt_path
-for seed in 42; do
-  for epoch in 100 500 1000 2000 5000; do
-    results_dir="${base_results_dir}seed_${seed}/epoch_${epoch}/"
-    # run the job
-    sbatch --time=100:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python train_ddpm.py --config_name $dataset --vae_chkpt_path $path --results_dir $results_dir --seed $seed --epochs $epoch"
-  done
-done
-
-
+# # loop over seeds and vae_chkpt_path
+# for seed in 42; do
+#   results_dir="${base_results_dir}seed_${seed}/epoch_${epoch}/"
+#   # run the job
+#   sbatch --time=100:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python train_ddpm.py --config_name $dataset --vae_chkpt_path $path --results_dir $results_dir --seed $seed"
+# done
