ef9d6f5f33385ea4bc8cf24b42e20b468b643067
JoGo175 <jorgefernando1998@hotmail.com>
2024-05-26 19:29:11 +0200

    pretraining notebook

---
+++ classifier_pretraining/cifar10_clf_training.py
@@ -0,0 +1,141 @@
+import os
+import sys
+import torch
+import torchvision
+import yaml
+import tqdm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+import torchvision.models as models
+
+from PIL import Image
+from omegaconf import OmegaConf
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import confusion_matrix
+
+# set directory to parent directory, one level up
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# print current working directory
+print("Current Working Directory:", os.getcwd())
+
+from utils.data_utils import get_data, get_gen
+
+
+# Configurations
+configs = {
+    "data": {
+        "data_name": "cifar10",
+        "num_clusters_data": 10,
+    },
+    "training": {
+        "batch_size": 256,
+        "augment": False,
+        "augmentation_method": 'simple',
+        "aug_decisions_weight": 1,
+    },
+    "globals": {
+        "seed": 42,
+    }
+}
+
+
+def main():
+    # Set seed
+    torch.manual_seed(configs['globals']['seed'])
+    np.random.seed(configs['globals']['seed'])
+
+    # Get data
+    trainset, trainset_eval, testset = get_data(configs)
+
+    gen_train = get_gen(trainset, configs, validation=False, shuffle=False)
+    gen_train_eval = get_gen(trainset_eval, configs, validation=True, shuffle=False)
+    gen_test = get_gen(testset, configs, validation=True, shuffle=False)
+
+    gen_train_eval_iter = iter(gen_train_eval)
+    gen_test_iter = iter(gen_test)
+
+    y_train = trainset_eval.dataset.targets.clone().detach()[trainset_eval.indices].numpy()
+    y_test = testset.dataset.targets.clone().detach()[testset.indices].numpy()
+
+    print("\nTrainset shape:", trainset.dataset.data.shape)
+    print("Number of samples in trainset:", len(gen_train.dataset))
+
+    print("\nTrainset eval shape:", trainset_eval.dataset.data.shape)
+    print("Number of samples in trainset eval:", len(gen_train_eval.dataset))
+
+    print("\nTestset shape:", testset.dataset.data.shape)
+    print("Number of samples in testset:", len(gen_test.dataset))
+
+    print(50 * "-")
+
+    # device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load model 
+    model = models.resnet50(pretrained=True)
+    model = model.to(device)
+
+    # Loss function and optimizer
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # Epochs
+    n_epochs = 50
+
+    # Training
+    model.train()
+    for epoch in tqdm.tqdm(range(n_epochs)):
+        for i, (x_batch, y_batch) in enumerate(gen_train):
+            x_batch, y_batch = x_batch.to(device), y_batch.to(device)
+
+            optimizer.zero_grad()
+
+            y_pred = model(x_batch)
+
+            loss = criterion(y_pred, y_batch)
+            loss.backward()
+            optimizer.step()
+
+            # use gen_train_eval to evaluate the model
+            if i % 10 == 0:
+                model.eval()
+                with torch.no_grad():
+                    x_batch_eval, y_batch_eval = next(gen_train_eval_iter)
+                    x_batch_eval, y_batch_eval = x_batch_eval.to(device), y_batch_eval.to(device)
+
+                    y_pred_eval = model(x_batch_eval)
+                    loss_eval = criterion(y_pred_eval, y_batch_eval)
+
+                    print(f"Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}, Loss eval: {loss_eval.item()}")
+                model.train()
+
+    # Save model
+    torch.save(model.state_dict(), f"classifier_pretraining/resnet50_{configs['data']['data_name']}.pth")
+
+    # Evaluation
+    model.eval()
+
+    with torch.no_grad():
+        for i, (x, y) in enumerate(gen_test):
+            if i == 0:
+                y_pred = model(x)
+            else:
+                y_pred = torch.cat([y_pred, model(x)], dim=0)
+
+    y_pred = y_pred.argmax(dim=1).numpy()
+
+    print("Accuracy:", np.mean(y_pred == y_test))
+    print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ classifier_pretraining/run_clf_pretraining.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --time=36:00:00
+eval "$(conda shell.bash hook)"
+# Needed for deterministic computations
+export CUBLAS_WORKSPACE_CONFIG=":4096:8"
+source ~/.bashrc
+conda activate treevae
+
+dataset="cifar10"
+
+O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
+
+
+# MNIST
+if [ "$dataset" == "mnist" ]; then
+    O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
+    sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/mnist_clf_training.py"
+fi
+
+# FashionMNIST
+if [ "$dataset" == "fmnist" ]; then
+    O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
+    sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/fmnist_clf_training.py"
+fi
+
+# CIFAR10
+if [ "$dataset" == "cifar10" ]; then
+    O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
+    sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/cifar10_clf_training.py"
+fi
