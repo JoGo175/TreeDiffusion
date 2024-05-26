5d7a0b9b5a4d8ef5fc84d0a7b8cfbb4933e61d02
JoGo175 <jorgefernando1998@hotmail.com>
2024-05-26 21:32:20 +0200

    pretraining classifier, all datasets

--- classifier_pretraining/run_clf_pretraining.sh
+++ classifier_pretraining/run_clf_pretraining.sh
@@ -11,20 +11,4 @@ dataset="cifar10"
 O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
 
 
-# MNIST
-if [ "$dataset" == "mnist" ]; then
-    O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
-    sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/mnist_clf_training.py"
-fi
-
-# FashionMNIST
-if [ "$dataset" == "fmnist" ]; then
-    O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
-    sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/fmnist_clf_training.py"
-fi
-
-# CIFAR10
-if [ "$dataset" == "cifar10" ]; then
-    O_DIR="/cluster/work/vogtlab/Group/jogoncalves/logs/output.%x.%J_${dataset}.out"
-    sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/cifar10_clf_training.py"
-fi
+sbatch --time=36:00:00 --mem-per-cpu=20G -p gpu --gres=gpu:1 -A vogtlab --tmp=20G --cpus-per-task=2 -o $O_DIR --wrap="python classifier_pretraining/clf_training.py --data_name $dataset"
