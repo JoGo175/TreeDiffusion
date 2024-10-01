4070a17c1a1cc71729ccb0070174483200c1dcce
JoGo175 <jorgefernando1998@hotmail.com>
2024-10-01 18:08:45 +0200

    cubicc vae fid

--- cubicc_FID.py
+++ cubicc_FID.py
@@ -16,10 +16,13 @@ from utils.data_utils import get_data, get_gen
 from utils.model_utils import construct_tree_fromnpy
 from utils.utils import reset_random_seeds, prepare_config
 from FID.fid_score import calculate_fid, get_precomputed_fid_scores_path, save_fid_stats_as_dict
+from utils.training_utils import compute_leaves, validate_one_epoch, Custom_Metrics, predict, move_to
+import gc
+import tqdm
 
 ###############################################################################################################
 # SELECT THE DATASET
-dataset = "celeba"       # mnist, fmnist, cifar10, celeba, cubicc is supported
+dataset = "cubicc"       # mnist, fmnist, cifar10, celeba, cubicc is supported
 ###############################################################################################################
 
 
@@ -84,23 +87,183 @@ def train():
 
     # setup device, mps is for M1 or M2 macs
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device = "mps"
+    # device = "mps"
 
-    if configs['data']['data_name'] in ['cubicc', 'celeba']:
-        train_data = np.concatenate([batch[0].permute(0, 2, 3, 1).numpy() for batch in
-                        torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False)])
-        test_data = np.concatenate([batch[0].permute(0, 2, 3, 1).numpy() for batch in
-                                     torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)])
-    else:
-        train_data = trainset.dataset.data
-        test_data = testset.dataset.data
-    # precompute or load fid scores for train and test
-    data_stats_train = get_precomputed_fid_scores_path(train_data, configs['data']['data_name'],
-                                                       subset="train", device=device)
-    data_stats_test = get_precomputed_fid_scores_path(test_data, configs['data']['data_name'], subset="test",
-                                                      device=device)
+    base_path = '/cluster/work/vogtlab/Group/jogoncalves/treevae/'
 
-    print("FID scores for train set:", data_stats_train)
+    path_1 = 'models/experiments/cubicc/20240923-013355_8ddcc'
+    path_2 = 'models/experiments/cubicc/20240923-013355_276bd'
+    path_3 = 'models/experiments/cubicc/20240923-013355_2538a'
+    path_4 = 'models/experiments/cubicc/20240923-013355_6236b'
+    path_5 = 'models/experiments/cubicc/20240923-013355_fc06f'
+    path_6 = 'models/experiments/cubicc/20240923-013355_434df'
+    path_7 = 'models/experiments/cubicc/20240923-013355_61ff6'
+    path_8 = 'models/experiments/cubicc/20240923-013355_ac769'
+    path_9 = 'models/experiments/cubicc/20240923-013340_34724'
+    path_10 = 'models/experiments/cubicc/20240923-013340_0e296'
+    vae_path_list = [path_1, path_2, path_3, path_4, path_5, path_6, path_7, path_8, path_9, path_10]
+
+    # lists to store FID scores for each seed
+    train_FID_generations_vae = []
+    test_FID_generations_vae = []
+    train_FID_reconstructions_vae = []
+    test_FID_reconstructions_vae= []
+
+    # load model
+    for m, model_folder in enumerate(vae_path_list):
+        checkpoint_path = base_path + model_folder
+
+        model = TreeVAE(**configs['training'])
+        data_tree = np.load(checkpoint_path + '/data_tree.npy', allow_pickle=True)
+        model = construct_tree_fromnpy(model, data_tree, configs)
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model.load_state_dict(torch.load(checkpoint_path + '/model_weights.pt', map_location=device), strict=False)
+        model.to(device)
+        model.eval()
+
+        data_stats_train = get_precomputed_fid_scores_path("placeholder", configs['data']['data_name'],
+                                                           batch_size=50, subset="train", device=device)
+        data_stats_test = get_precomputed_fid_scores_path("placeholder", configs['data']['data_name'],
+                                                          batch_size=50, subset="test", device=device)
+
+        # Generations FID -----------------------------------------------------------
+
+        # generate 10k samples from the model
+        n_imgs = 10000
+        with torch.no_grad():
+            generations, p_c_z = model.generate_images(n_imgs, device)
+        generations = move_to(generations, 'cpu')
+        p_c_z = move_to(p_c_z, 'cpu')
+
+        # for each generated image, only save the ones that are in the leaf with the highest probability
+        generations_list = []
+        for i in range(n_imgs):
+            # only save generation from leaf with highest probability
+            leaf_ind = torch.argmax(p_c_z[i])
+            generations_list.append(generations[leaf_ind][i])
+        gen_dataset = torch.stack(generations_list).squeeze()
+        _ = gc.collect()
+
+        # compute FID score for generated images
+
+        # precompute FID scores for generated images
+        stats_generations = save_fid_stats_as_dict(gen_dataset, batch_size=50, device=device, dims=2048)
+        train_FID_generations = calculate_fid([data_stats_train, stats_generations], batch_size=50, device=device,
+                                              dims=2048)
+        test_FID_generations = calculate_fid([data_stats_test, stats_generations], batch_size=50, device=device,
+                                             dims=2048)
+
+        print("FID score for generated images compared to train set:", train_FID_generations)
+        print("FID score for generated images compared to test set:", test_FID_generations)
+        train_FID_generations_vae.append(train_FID_generations)
+        test_FID_generations_vae.append(test_FID_generations)
+        _ = gc.collect()
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+        # Reconstructions FID -----------------------------------------------------------
+
+        # only compute reconstruction FID for test set in colored images to reduce cuda memory usage
+        if configs['data']['data_name'] in ["cifar10", "cubicc", "celeba"]:
+            fid_eval_set = ['test']
+        else:
+            fid_eval_set = ['train', 'test']
+
+        for subset in fid_eval_set:
+            reconstructions_list = []
+
+            if subset == 'train':
+                gen_train_eval = get_gen(trainset, configs, validation=True, shuffle=False)
+                data_loader = gen_train_eval
+            elif subset == 'test':
+                gen_test = get_gen(testset, configs, validation=True, shuffle=False)
+                data_loader = gen_test
+
+            for inputs, labels in tqdm(data_loader):
+                inputs_gpu, labels_gpu = inputs.to(device), labels.to(device)
+                with torch.no_grad():
+                    reconstructions, node_leaves = model.compute_reconstruction(inputs_gpu)
+                reconstructions = move_to(reconstructions, 'cpu')
+                node_leaves = move_to(node_leaves, 'cpu')
+                _ = gc.collect()
+
+                # add reconstruction to list
+                for i in range(len(inputs)):
+                    # probs are the probabilities of each leaf for the data point i
+                    probs = [node_leaves[j]['prob'][i] for j in range(len(node_leaves))]
+                    # use the leaf with highest probability
+                    leaf_ind = torch.argmax(torch.tensor(probs))
+                    # add reconstruction to list
+                    reconstructions_list.append(reconstructions[leaf_ind][i])
+            reconstructions_dataset = torch.stack(reconstructions_list).squeeze().detach()
+            _ = gc.collect()
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+
+            if configs['data']['data_name'] in ["cifar10", "cubicc", "celeba"]:
+                batch_size_fid = 25
+            else:
+                batch_size_fid = 50
+
+            # precompute FID scores for generated images
+            stats_reconstructions = save_fid_stats_as_dict(reconstructions_dataset, batch_size=batch_size_fid,
+                                                           device=device,
+                                                           dims=2048)
+            _ = gc.collect()
+
+            if subset == 'train':
+                train_FID_reconstructions = calculate_fid([data_stats_train, stats_reconstructions],
+                                                          batch_size=batch_size_fid,
+                                                          device=device, dims=2048)
+                print("FID score for reconstructed images, train set:", train_FID_reconstructions)
+                train_FID_reconstructions_vae.append(train_FID_reconstructions)
+            elif subset == 'test':
+                test_FID_reconstructions = calculate_fid([data_stats_test, stats_reconstructions],
+                                                         batch_size=batch_size_fid,
+                                                         device=device, dims=2048)
+                print("FID score for reconstructed images, test set:", test_FID_reconstructions)
+                test_FID_reconstructions_vae.append(test_FID_reconstructions)
+            _ = gc.collect()
+
+    print("\nFID scores for DDPM samples compared to train set:\n", train_FID_generations_vae)
+    print("\nFID scores for DDPM samples compared to test set:\n", test_FID_generations_vae)
+    print("\nFID scores for DDPM reconstructions compared to train set:\n", train_FID_reconstructions_vae)
+    print("\nFID scores for DDPM reconstructions compared to test set:\n", test_FID_reconstructions_vae)
+
+    # compute average and standard deviation of FID scores for DDPM
+    print(configs['data']['data_name'])
+    print("-----------------------------------")
+    print("Reconstructions")
+    print("\nAverage FID score for DDPM reconstructions compared to test set:",
+          np.mean(test_FID_reconstructions_vae))
+    print("\nStandard deviation FID score for DDPM reconstructions compared to test set:",
+          np.std(test_FID_reconstructions_vae))
+    print("-----------------------------------")
+    print("Generations")
+    print("\nAverage FID score for DDPM samples compared to test set:", np.mean(test_FID_generations_vae))
+    print("\nStandard deviation FID score for DDPM samples compared to test set:",
+          np.std(test_FID_generations_vae))
+
+
+
+
+    #
+    # if configs['data']['data_name'] in ['cubicc', 'celeba']:
+    #     train_data = np.concatenate([batch[0].permute(0, 2, 3, 1).numpy() for batch in
+    #                     torch.utils.data.DataLoader(trainset, batch_size=32, shuffle=False)])
+    #     test_data = np.concatenate([batch[0].permute(0, 2, 3, 1).numpy() for batch in
+    #                                  torch.utils.data.DataLoader(testset, batch_size=32, shuffle=False)])
+    # else:
+    #     train_data = trainset.dataset.data
+    #     test_data = testset.dataset.data
+    # # precompute or load fid scores for train and test
+    # data_stats_train = get_precomputed_fid_scores_path(train_data, configs['data']['data_name'],
+    #                                                    subset="train", device=device)
+    # data_stats_test = get_precomputed_fid_scores_path(test_data, configs['data']['data_name'], subset="test",
+    #                                                   device=device)
+    #
+    # print("FID scores for train set:", data_stats_train)
     # print("FID scores for test set:", data_stats_test)
     #
     # # paths to the generated images
